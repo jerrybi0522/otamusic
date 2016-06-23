963d065106f439f538a18c2297c37e4b646c58bc
Jerry Bi <jerrybi0522@gmail.com>
2016-06-23 11:03:15 -0400

    adding ujs to favorites buttons

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -2,16 +2,23 @@ class FavoritesController < ApplicationController
   def create
   	@favorite = Favorite.new(user_id: current_user.id, song_id: params[:song_id])
   	@song = Song.find(params[:song_id])
-  	@favorite.save
-  	# Change to ujs
-  	redirect_to song_path(@song)
+    respond_to do |format|
+  	  if @favorite.save
+        format.js
+      else
+        format.html { redirect_to :back }
+      end
+    end
+  	# redirect_to song_path(@song)
   end
 
   def destroy
   	@favorite = Favorite.find(params[:id])
   	@song = Song.find(@favorite.song_id)
   	@favorite.destroy
-  	# Change to ujs
-  	redirect_to song_path(@song)
+    respond_to do |format|
+      format.js
+    end
+  	# redirect_to song_path(@song)
   end
 end
