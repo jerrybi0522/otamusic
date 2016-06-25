8330fa4257d126eca2baf252da440e7694e08ee6
Jerry Bi <jerrybi0522@gmail.com>
2016-06-25 16:47:48 -0400

    Styling videos, text boxes, descriptions, and links

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,21 +21,51 @@ $(document).ready(function(){
 		$('body').css({
 			"background-color": "#ffcce6"
 		});
+		$('.youtube_video').css({
+			"padding": "10px",
+			"background-color" : "#F887CB",
+			"width": "67.5%",
+			"margin": "auto"
+		});
 	} else if ($("#song_title").data('cat') == 2){
 		$('body').css({
 			"background-color": "#fff0b3"
+		});
+		$('.youtube_video').css({
+			"padding": "10px",
+			"background-color" : "#FFC809",
+			"width": "67.5%",
+			"margin": "auto"
 		});
 	} else if ($("#song_title").data('cat') == 3){
 		$('body').css({
 			"background-color": "#b3ecff"
 		});
+		$('.youtube_video').css({
+			"padding": "10px",
+			"background-color" : "#0DB7FF",
+			"width": "67.5%",
+			"margin": "auto"
+		});
 	} else if ($("#song_title").data('cat') == 4){
 		$('body').css({
 			"background-color": "#ff9999"
 		});
+		$('.youtube_video').css({
+			"padding": "10px",
+			"background-color" : "#0DB7FF",
+			"width": "67.5%",
+			"margin": "auto"
+		});
 	} else if ($("#song_title").data('cat') == 5){
 		$('body').css({
 			"background-color": "#c6ffb3"
+		});
+		$('.youtube_video').css({
+			"padding": "10px",
+			"background-color" : "#80FF00",
+			"width": "67.5%",
+			"margin": "auto"
 		});
 	}
 });
